9d620ff3c11c786ec50754bd1644098c5e9caff2
xschen <xs0040@gmail.com>
2018-01-11 10:00:53 +0800

    update build scripts

--- make.ps1
+++ make.ps1
@@ -14,4 +14,19 @@ $source=$PSScriptRoot + "/target/" + $proj + ".jar"
 $dest=$PSScriptRoot + "/kafka-standalone.jar"
 copy $source $dest
 
+$client_path = $PSScriptRoot + "/java-kafka-client-demo"
+cd $client_path
+
+Invoke-Expression -Command:"mvn -f pom.xml clean package -P$profile -U"
+
+$proj="java-kafka-consumer-demo"
+$source=$client_path + "/" +$proj + "/target/" + $proj + ".jar"
+$dest=$PSScriptRoot + "/kafka-consumer-demo.jar"
+copy $source $dest
+
+$proj="java-kafka-producer-demo"
+$source=$client_path + "/" +$proj + "/target/" + $proj + ".jar"
+$dest=$PSScriptRoot + "/kafka-producer-demo.jar"
+copy $source $dest
+
 cd $currentPath
--- make.sh
+++ make.sh
@@ -3,3 +3,13 @@
 mvn -f pom.xml clean package -Plocal -U
 
 cp /target/java-kafka-standalone-server.jar kafka-standalone.jar
+
+cd java-kafka-client-demo
+
+mvn -f pom.xml clean package -Plocal -U
+
+cp /target/java-kafka-consumer-demo.jar ../kafka-consumer-demo.jar
+
+cp /target/java-kafka-producer-demo.jar ../kafka-producer-demo.jar
+
+cd ..
